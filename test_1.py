0e5d2d8526606e4650bdb42465187785829c3325
Carlos Gomes <clogomes@Carloss-Laptop.lan>
2025-12-14 18:11:39 +0000

    Adiciona app.py com script inicial

---
+++ test_1.py
@@ -0,0 +1,6 @@
+# Definindo as variáveis
+nome = "Carlos"
+projeto = "first-repo"
+
+# Imprimindo a mensagem usando f-string para formatar
+print(f"Olá, {nome}! O seu projeto no GitHub ({projeto}) está ligado com sucesso.")
